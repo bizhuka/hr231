b5120e4ec304733f8f88462b1459881cccca2f52
Birzhan Moldabayev <36256417+bizhuka@users.noreply.github.com>
2023-05-18 09:55:31 +0500

    minor change

---
+++ webapp/ext/controller/SPCalendar.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/m/SinglePlanningCalendar"
+], function (SinglePlanningCalendar) {
+	"use strict";
+
+	return SinglePlanningCalendar.extend("zhr231.ext.controller.SPCalendar", {
+		// metadata : { },
+		// init : function () { },        
+		// renderer : { }
+	});
+});
